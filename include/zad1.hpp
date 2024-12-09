7a24de0f2bc99cebe24baf97ce123086cc88bef0
Zuzanna Żakowska <zakowska.zuzanna@gmail.com>
2024-12-10 00:28:20 +0100

    Kolokwium treningowe

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -1,8 +1,20 @@
 #include "catch.hpp"
 
-// Zad2
-// tutaj definicja klasy Makaron
-// BEZ definicji metody gotujMakaron (tylko z deklaracją)
+class Makaron
+{
+public:
+    virtual ~Makaron()                      = default;
+    virtual double  ileMaki(unsigned) const = 0;
+    static Makaron* gotujMakaron(const std::string& s);
+};
 
-// Zad1
-// tutaj definicja klasy Tagliatelle
+class Tagliatelle : public Makaron
+{
+    double              L, W, R;
+    static const double C;
+
+public:
+    Tagliatelle() : L{.5}, W{.5}, R{.5} {}
+    Tagliatelle(double a, double b, double c) : L{a}, W{b}, R{c} {}
+    double ileMaki(unsigned P) const { return static_cast< double >(P) * L * W * (1. - R) * C; }
+};
--- include/zad4.hpp
+++ include/zad4.hpp
@@ -2,4 +2,15 @@
 
 #include <vector>
 
-// tutaj funkcja obliczMake
+double obliczMake(const std::vector< Tagliatelle >& v)
+{
+    double      M = 0.;
+    std::size_t i = 1u;
+    for (auto it = v.crbegin(); it != v.crend(); ++it)
+        M += it->ileMaki(i++);
+    if (M > 100.)
+        throw 1;
+    if (M > 50.)
+        throw 1.;
+    return M;
+}
